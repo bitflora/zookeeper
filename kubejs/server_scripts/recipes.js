e8e17b8e2931e71b811b3f14933f63c3cc557775
Jask <xxx_darkraven_xxx@protonmail.com>
2025-05-03 11:55:00 -0400

    more marine_food taming

--- kubejs/server_scripts/recipes.js
+++ kubejs/server_scripts/recipes.js
@@ -9,19 +9,68 @@ ServerEvents.recipes(e => { //listen for the "recipes" server event.
     // This part, inside the curly braces, is the callback.
     // You can modify as many recipes as you like in here,
     // without needing to use ServerEvents.recipes() again.
-      
+
     console.log('Hello! The recipe event has fired!');
 
+    // Raise the cobblestone requirements
+    e.remove({ output: 'minecraft:dropper' });
+    e.shaped('1x minecraft:dropper', [
+        'CCC',
+        'C C',
+        'CRC'
+      ], {
+        C: 'compressedblocks:c1_cobblestone',
+        R: 'minecraft:redstone',
+      }
+    );
+
+    e.remove({ output: 'minecraft:dispenser' });
+    e.shaped('1x minecraft:dispenser', [
+        'CCC',
+        'CBC',
+        'CRC'
+      ], {
+        C: 'compressedblocks:c1_cobblestone',
+        B: 'minecraft:bow',
+        R: 'minecraft:redstone',
+      }
+    );
+
+    e.remove({ output: 'minecraft:observer' });
+    e.shaped('1x minecraft:observer', [
+        'CCC',
+        'RRQ',
+        'CCC'
+      ], {
+        C: 'compressedblocks:c1_cobblestone',
+        R: 'minecraft:redstone',
+        Q: 'minecraft:quartz'
+      }
+    );
+
+    e.remove({ output: 'minecraft:repeater' });
+    e.shaped('1x minecraft:repeater', [
+        '   ',
+        'TRT',
+        'CCC'
+      ], {
+        C: 'compressedblocks:c1_cobblestone',
+        R: 'minecraft:redstone',
+        T: 'minecraft:redstone_torch'
+      }
+    );
+
+    // Custom items
     e.smelting('2x supplementaries:ash', 'compressedblocks:c1_cobblestone');
 
     e.shapeless('kubejs:marine_food', [
         'minecraft:sugar',
-        'aquaculture:worm',
+        '2x aquaculture:worm',
         '3x supplementaries:ash'
     ]);
     e.shapeless('kubejs:marine_food', [
         'minecraft:sugar',
-        'alexs_herps_:mealworm',
+        '2x alexs_herps_:mealworm',
         '3x supplementaries:ash'
     ]);
-  })
+  });
