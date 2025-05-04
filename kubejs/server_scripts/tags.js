cddc8f4c79b6fa8955636d3e017e6728b135ec32
Jask <xxx_darkraven_xxx@protonmail.com>
2025-05-04 14:46:53 -0400

    data pack that I may do in kubejs instead

---
+++ kubejs/server_scripts/tags.js
@@ -0,0 +1,7 @@
+ServerEvents.tags('item', e => {
+    e.add('kubejs:furs', 'alexsmobs:bear_fur');
+    e.add('kubejs:furs', 'alexsmobs:bison_fur');
+    e.add('kubejs:furs', 'biomesoplenty:hair');
+    e.add('kubejs:furs', 'naturalist:fur');
+    e.add('kubejs:furs', 'unusualprehistory:smilo_fur');
+});
