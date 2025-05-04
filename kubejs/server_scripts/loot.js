49040cdc790a2f019e0094a9dbfd5227b43cbd0d
Jask <xxx_darkraven_xxx@protonmail.com>
2025-05-04 19:48:12 -0400

    This time loot is showing up.

---
+++ kubejs/server_scripts/loot.js
@@ -0,0 +1,16 @@
+LootJS.modifiers((e) => {
+    let furries = [
+        'alexsmobs:raccoon',
+        'alexsmobs:gazelle',
+        'alexsmobs:anteater',
+        'alexsmobs:capuchin_monkey',
+        'alexsmobs:gelada_monkey',
+        'alexsmobs:gorilla',
+        'alexsmobs:tiger',
+    ];
+    // Currently electing to do this through the datapack instead
+    // furries.forEach(function (furry) {
+    //     e.addEntityLootModifier(furry)
+    //         .addLoot('naturalist:fur');
+    // });
+});
--- kubejs/server_scripts/recipes.js
+++ kubejs/server_scripts/recipes.js
@@ -60,9 +60,36 @@ ServerEvents.recipes(e => { //listen for the "recipes" server event.
       }
     );
 
+    // Remove some recipes
+    e.remove({ mod: 'cyclic' })
+    e.remove({ output: 'theoneprobe:probe' });
+
+    // add back in select Cyclic items
+    e.shaped('1x cyclic:clock', [
+        'CCC',
+        'FLT',
+        'CCC'
+      ], {
+        C: 'compressedblocks:c1_cobblestone',
+        F: 'kubejs:fur_ball',
+        L: 'minecraft:clock',
+        T: 'minecraft:redstone_torch'
+      }
+    );
+
+    e.shaped('cyclic:sleeping_mat', [
+        '   ',
+        'FFB',
+        'FFF'
+    ], {
+        F: '#kubejs:furs',
+        B: 'kubejs:fur_ball'
+    });
+
     // Custom items
     e.smelting('2x supplementaries:ash', 'compressedblocks:c1_cobblestone');
 
+    // Marine Food
     e.shapeless('kubejs:marine_food', [
         'minecraft:sugar',
         '2x aquaculture:worm',
@@ -70,7 +97,31 @@ ServerEvents.recipes(e => { //listen for the "recipes" server event.
     ]);
     e.shapeless('kubejs:marine_food', [
         'minecraft:sugar',
-        '2x alexs_herps_:mealworm',
+        '3x alexs_herps_:mealworm',
         '3x supplementaries:ash'
     ]);
+    e.shapeless('kubejs:marine_food', [
+        'minecraft:sugar',
+        '3x alexsmobs:maggot',
+        '3x supplementaries:ash'
+    ]);
+
+    e.shapeless('2x alexs_herps_:burrowed_soil', [
+        'alexs_herps_:burrowed_soil',
+        'minecraft:dirt',
+        '#minecraft:leaves'
+    ]);
+    e.shapeless('2x alexs_herps_:burrowed_soil', [
+        'alexs_herps_:burrowed_soil',
+        'minecraft:dirt',
+        'farmersdelight:straw'
+    ]);
+
+    // Fur ball
+    e.shapeless('kubejs:fur_ball', [
+        '#minecraft:wool',
+        '#kubejs:furs',
+        '#forge:feathers'
+    ]);
+
   });
