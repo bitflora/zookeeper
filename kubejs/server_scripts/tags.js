4efc9af98c3a5970d7624e7fe346733e7d6110ca
Jask <xxx_darkraven_xxx@protonmail.com>
2025-05-08 18:24:44 -0400

    renamed quest files, more quest stuff

--- kubejs/server_scripts/tags.js
+++ kubejs/server_scripts/tags.js
@@ -5,6 +5,33 @@ ServerEvents.tags('item', e => {
     e.add('kubejs:furs', 'naturalist:fur');
     e.add('kubejs:furs', 'unusualprehistory:smilo_fur');
 
+    e.add('kubejs:mammal_meat', '#forge:raw_pork');
+    e.add('kubejs:mammal_meat', 'minecraft:beef');
+    e.add('kubejs:mammal_meat', 'minecraft:porkchop');
+    e.add('kubejs:mammal_meat', 'minecraft:mutton');
+    e.add('kubejs:mammal_meat', 'minecraft:rabbit');
+    e.add('kubejs:mammal_meat', 'naturalist:bushmeat');
+    e.add('kubejs:mammal_meat', 'naturalist:venison');
+    e.add('kubejs:mammal_meat', 'farmersdelight:minced_beef');
+    e.add('kubejs:mammal_meat', 'alexsmobs:kangaroo_meat');
+    e.add('kubejs:mammal_meat', 'alexsdelight:raw_bison');
+    e.add('kubejs:mammal_meat', 'unusualprehistory:raw_mammoth');
+    e.add('kubejs:mammal_meat', 'rats:raw_rat');
+    e.add('kubejs:mammal_meat', 'eanimod:rawrabbit_small');
+
+    e.add('kubejs:bird_meat', '#forge:raw_chicken');
+    e.add('kubejs:bird_meat', 'minecraft:chicken');
+    e.add('kubejs:bird_meat', 'exoticbirds:raw_birdmeat');
+    e.add('kubejs:bird_meat', 'eanimod:rawchicken_darksmall');
+    e.add('kubejs:bird_meat', 'eanimod:rawchicken_dark');
+    e.add('kubejs:bird_meat', 'eanimod:rawchicken_darkbig');
+    e.add('kubejs:bird_meat', 'eanimod:rawchicken_palesmall');
+    e.add('kubejs:bird_meat', 'eanimod:rawchicken_pale');
+    e.add('kubejs:bird_meat', 'farmersdelight:chicken_cuts');
+    e.add('kubejs:bird_meat', 'naturalist:duck');
+
+    // already have forge:raw_fishes
+
     e.add('forge:feathers', 'alexsmobs:roadrunner_feather');
     e.add('forge:feathers', 'alexsmobs:emu_feather');
     e.add('forge:feathers', 'unusualprehistory:raptor_feathers');
